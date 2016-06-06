67d6d6394d7034e0b9982e99abb0a19daddb2bc0
Pedro Costa <pedroxenix@gmail.coma>
2016-06-06 07:54:20 +0200

    Completed boat class. Added specs

--- boat.rb
+++ boat.rb
@@ -1,16 +1,20 @@
 class Boat
+  MAX_SIZE = 3
+
   attr_accessor :size, :health, :orientation, :row, :column
 
   # orientation= :vertical | :horizontal
-  def initialize size, orientation: :vertical
+  def initialize size
     @size = size
     @health = size
-    @orientation = orientation
   end
 
-  def set_location row, column
+  # set the placement of the boat in the board
+  #  set row, column and orientation of the boat
+  def set_location row, column, orientation
     @row = row
     @column = column
+    @orientation = orientation
   end
 
   # boat gets hit by a torpedo
@@ -18,4 +22,28 @@ class Boat
     @health -= 1
     @health
   end
+
+  # given the coordinates of a shot, returns true if it was a shot miss, or false if it was a hit
+  def shot_missed? row, column
+    (0...@size).each do |offset|
+      if @orientation == :horizontal
+        return false if column == (@column+offset)
+      elsif @orientation == :vertical
+        return false if row == (@row+offset)
+      end
+    end
+    true
+  end
+
+  private
+
+  # given the coordinates of a shot, return if it is possible that the boat was hit
+  def possible_hit? row, column
+    if @orientation == :horizontal
+      return @row == row
+    elsif @orientation == :vertical
+      return @column == column
+    end
+  end
+
 end
--- spec/boat_spec.rb
+++ spec/boat_spec.rb
@@ -1,15 +1,15 @@
 require_relative '../boat'
 
 describe Boat do
-  it "should initialize a boat with correct health, size" do
-    boat = Boat.new(3)
 
+  let(:boat) { Boat.new(3) }
+
+  it "should initialize a boat with correct health, size" do
     expect(boat.size).to eq(3)
     expect(boat.health).to eq(3)
   end
 
   it "should decrease health by one after a hit" do
-    boat = Boat.new(3)
     boat.take_hit
 
     expect(boat.health).to eq(2)
@@ -19,7 +19,6 @@ describe Boat do
   end
 
   it "should decrease health by one after a hit" do
-    boat = Boat.new(3)
     boat.take_hit
 
     expect(boat.health).to eq(2)
@@ -29,10 +28,94 @@ describe Boat do
   end
 
   it "should return available health after a hit" do
-    boat = Boat.new(3)
-
     expect(boat.take_hit).to eq(2)
 
     expect(boat.take_hit).to eq(1)
+  end
+
+  describe "private methods" do
+    describe "shot_missed?" do
+      it "should return true if the shot was a miss(horizontal)" do
+        # make boat will be on (0,0) (0,1)
+        boat.row = 0
+        boat.column = 0
+        boat.size = 2
+        boat.orientation = :horizontal
+
+        expect(boat.send(:shot_missed?,0,2)).to be true
+      end
+
+      it "should return true if the shot was a miss(vertical)" do
+        # make boat will be on (0,0) (1,0)
+        boat.row = 0
+        boat.column = 0
+        boat.size = 2
+        boat.orientation = :vertical
+
+        expect(boat.send(:shot_missed?,2,0)).to be true
+      end
+
+      it "should return false if the shot was a hit(horizontal)" do
+        # make boat will be on (0,0) (0,1)
+        boat.row = 0
+        boat.column = 0
+        boat.size = 2
+        boat.orientation = :horizontal
+
+        expect(boat.send(:shot_missed?,0,1)).to be false
+      end
+
+      it "should return false if the shot was a miss(vertical)" do
+        # make boat will be on (0,0) (1,0)
+        boat.row = 0
+        boat.column = 0
+        boat.size = 2
+        boat.orientation = :vertical
+
+        expect(boat.send(:shot_missed?,1,0)).to be false
+      end
+    end
+
+    describe "possible_hit?" do
+      it "should return false if it is impossible for the boat to be in the shot coordinates(horizontal)" do
+        # make boat will be on (0,0) (0,1)
+        boat.row = 0
+        boat.column = 0
+        boat.size = 2
+        boat.orientation = :horizontal
+
+        expect(boat.send(:possible_hit?, 1, 0)).to be false
+      end
+
+      it "should return false if it is impossible for the boat to be in the shot coordinates(vertical)" do
+        # make boat will be on (0,0) (1,0)
+        boat.row = 0
+        boat.column = 0
+        boat.size = 2
+        boat.orientation = :vertical
+
+        expect(boat.send(:possible_hit?, 0, 1)).to be false
+      end
+
+      it "should return true if it is possible for the boat to be in the shot coordinates(horizontal)" do
+        # make boat will be on (0,0) (0,1)
+        boat.row = 0
+        boat.column = 0
+        boat.size = 2
+        boat.orientation = :horizontal
+
+        expect(boat.send(:possible_hit?, 0, 1)).to be true
+      end
+
+      it "should return true if it is possible for the boat to be in the shot coordinates(vertical)" do
+        # make boat will be on (0,0) (1,0)
+        boat.row = 0
+        boat.column = 0
+        boat.size = 2
+        boat.orientation = :vertical
+
+        expect(boat.send(:possible_hit?, 1, 0)).to be true
+      end
+    end
   end
 end
