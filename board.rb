687bd96441cbf00140f74de51bc79d02c4fb7de6
Pedro Costa <pedroxenix@gmail.coma>
2016-06-06 07:53:37 +0200

    Completed board. added board specs

--- board.rb
+++ board.rb
@@ -1,27 +1,32 @@
+require_relative 'boat'
 class Board
   # some macros for the grid
   NO_BOAT = 0
   BOAT_PRESENT = 1
-  ALREADY_HIT = -1
+  HIT = -1
 
   attr_accessor :grid, :boats
 
   BOARD_SIZE = 6
 
-  def initialize boats
+  def initialize
     height = BOARD_SIZE
     width = BOARD_SIZE
     # initialize a 2d array with all values set to 0
     @grid = Array.new(height, NO_BOAT) { Array.new(width, NO_BOAT) }
-    @boats = boats
+    @boats = []
   end
 
   # place boat on the board
   # boat will be placed from left to right or from top to bottom
   def place_boat boat, row, column, orientation: :horizontal
+    # check if boat can be placed on board
     raise "Out of bounds!" unless check_placement_bounds(boat.size, row, column, orientation)
+    raise "Cannot place boat here. Boats overlapped!" unless check_free_space(boat.size, row, column, orientation)
 
-    # lets mark the boat placement
+    # place boat on board
+    boat.set_location(row, column, orientation)
+    # lets mark the boat placement on the board
     (0...boat.size).each do |offset|
       if orientation == :horizontal
         @grid[row][column + offset] = BOAT_PRESENT
@@ -29,11 +34,13 @@ class Board
         @grid[row + offset][column] = BOAT_PRESENT
       end
     end
+    @boats << boat
   end
 
   # mark a hit on the board
   def mark_hit row, column
-
+    mark_boat(row, column) if @grid[row][column] == BOAT_PRESENT
+    @grid[row][column] = HIT
   end
 
   private
@@ -50,7 +57,31 @@ class Board
     true
   end
 
-  # method to, given the coordinates of the shot, find the boar place in there
-  def find_boat row, columns
+  # Check if there is free space to place the boat
+  #  ie: boats do not overlap
+  def check_free_space boat_size, row, column, orientation
+    if orientation == :horizontal
+      (0...boat_size).each do |column_offset|
+        return false if @grid[row][column + column_offset] == BOAT_PRESENT
+      end
+    else
+      (0...boat_size).each do |row_offset|
+        return false if @grid[row + row_offset][column] == BOAT_PRESENT
+      end
+    end
+    true
   end
+
+  # method to, given the coordinates of the shot, find if a boat is placed in there
+  #  if a boat is present, make it take a hit
+  def mark_boat row, column
+    max_offset = Boat::MAX_SIZE
+
+    @boats.each do |boat|
+      unless boat.shot_missed?(row, column)
+        boat.take_hit
+      end
+    end
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,34 +2,24 @@ require_relative '../board'
 require_relative '../boat'
 
 describe Board do
+  let(:board) {Board.new}
+
   describe "initializer" do
     it "should initialize a new grid" do
-      boats = [Boat.new(3)]
-      board = Board.new(boats)
-
       expect(board.grid.class).to eq(Array)
     end
 
     it "should initialize a new grid with height of 6" do
-      boats = [Boat.new(3)]
-      board = Board.new(boats)
-
       # height must be 6
       expect(board.grid.size).to eq(6)
     end
 
     it "should initialize a new grid with width of 6" do
-      boats = [Boat.new(3)]
-      board = Board.new(boats)
-
       expect(board.grid[1].size).to eq(6)
     end
 
     # the boundaries are 6x6
     it "should initialize a new grid within the boundaries" do
-      boats = [Boat.new(3)]
-      board = Board.new(boats)
-
       expect(board.grid[5][5]).to eq(0)
       # [7][7] should not exist, and throw an error
       expect {
@@ -43,7 +33,8 @@ describe Board do
     it "should set on the grid the boat placement" do
       boat = Boat.new(2)
       boats = [boat]
-      board = Board.new(boats)
+      board = Board.new
+      board.boats = boats
 
       board.place_boat(boat, 0, 0, orientation: :horizontal)
 
@@ -55,7 +46,8 @@ describe Board do
     it "should set on the grid the boat placement vertically" do
       boat = Boat.new(2)
       boats = [boat]
-      board = Board.new(boats)
+      board = Board.new
+      board.boats = boats
 
       board.place_boat(boat, 0, 0, orientation: :vertical)
 
@@ -67,24 +59,45 @@ describe Board do
 
   describe "private methods" do
     it "should check the placement bounds and return true for a 2 sized boat placed on 0x0" do
-      boats = [Boat.new(2)]
-      board = Board.new(boats)
-
       expect( board.send(:check_placement_bounds, 2, 0, 0, :horizontal)).to eq(true)
     end
 
     it "should check the placement bounds and return true for a 6 sized boat placed on 0x0" do
-      boats = [Boat.new(2)]
-      board = Board.new(boats)
-
       expect( board.send(:check_placement_bounds, 6, 0, 0, :horizontal)).to eq(true)
     end
 
     it "should check the placement bounds and return false for a 7 sized boat placed on 0x0" do
-      boats = [Boat.new(2)]
-      board = Board.new(boats)
-
       expect( board.send(:check_placement_bounds, 7, 0, 0, :horizontal)).to eq(false)
+    end
+
+    it "should return false because the boat to be placed overlaps completely the other boat" do
+      # horizontal boat
+      board.grid[0][0] = board.grid[0][1] = Board::BOAT_PRESENT
+      # vertical boat
+      board.grid[3][5] = board.grid[4][5] = Board::BOAT_PRESENT
+
+      expect( board.send(:check_free_space, 2, 0, 0, :horizontal)).to eq(false)
+      expect( board.send(:check_free_space, 2, 3, 5, :vertical)).to eq(false)
+    end
+
+    it "should return false because the boat overlaps another boat" do
+      # horizontal boat
+      board.grid[0][2] = board.grid[0][3] = Board::BOAT_PRESENT
+      # vertical boat
+      board.grid[4][5] = board.grid[5][5] = Board::BOAT_PRESENT
+
+      expect( board.send(:check_free_space, 3, 0, 0, :horizontal)).to eq(false)
+      expect( board.send(:check_free_space, 3, 2, 5, :vertical)).to eq(false)
+    end
+
+    it "should return true because the boats do not overlap" do
+      # horizontal boat
+      board.grid[0][0] = board.grid[0][1] = Board::BOAT_PRESENT
+      # vertical boat
+      board.grid[4][5] = board.grid[5][5] = Board::BOAT_PRESENT
+
+      expect( board.send(:check_free_space, 3, 0, 2, :horizontal)).to eq(true)
+      expect( board.send(:check_free_space, 3, 1, 5, :horizontal)).to eq(true)
     end
   end
 end
