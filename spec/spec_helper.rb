1a7d16bb04b971e2d303077d3a937bf716f1e928
Pedro Costa <pedroxenix@gmail.coma>
2016-06-06 23:30:01 +0200

    updated readme. updated spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,6 @@ $: << File.expand_path("..",File.dirname(__FILE__))
 require 'board'
 require 'boat'
 require 'player'
-require 'game'
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
